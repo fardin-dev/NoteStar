d0c81ecbae46783de06f5b54c60f437787c07783
Fardin Sadman <fardin.sadman@aa.com>
2024-02-18 11:28:19 -0600

    update navigation

--- app/src/main/java/com/example/notestar/Navigation.kt
+++ app/src/main/java/com/example/notestar/Navigation.kt
@@ -1,10 +1,15 @@
 package com.example.notestar
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import com.example.notestar.detail.DetailScreen
 import com.example.notestar.home.HomeScreen
 import com.example.notestar.login.LoginScreen
 import com.example.notestar.login.SignUpScreen
@@ -19,23 +24,42 @@ enum class HomeRoutes {
     Detail
 }
 
+enum class NestedRoutes {
+    Main,
+    Login
+}
+
 @Composable
 fun Navigation(
     navController: NavHostController = rememberNavController(),
 ) {
-    NavHost(navController = navController, startDestination = LoginRoutes.SignIn.name) {
+    NavHost(
+        navController = navController,
+        startDestination = NestedRoutes.Main.name
+    ) {
+        authGraph(navController)
+        homeGraph(navController)
+    }
+}
+
+
+fun NavGraphBuilder.authGraph(navController: NavHostController) {
+    navigation(
+        startDestination = LoginRoutes.SignIn.name,
+        route = NestedRoutes.Login.name
+    ) {
         composable(route = LoginRoutes.SignIn.name) {
             LoginScreen(onNavToHomePage = {
-                navController.navigate(HomeRoutes.Home.name){
+                navController.navigate(NestedRoutes.Main.name) {
                     launchSingleTop = true
-                    popUpTo(LoginRoutes.SignIn.name){
+                    popUpTo(LoginRoutes.SignIn.name) {
                         inclusive = true
                     }
                 }
             }) {
-                navController.navigate(LoginRoutes.Signup.name){
+                navController.navigate(LoginRoutes.Signup.name) {
                     launchSingleTop = true
-                    popUpTo(LoginRoutes.SignIn.name){
+                    popUpTo(LoginRoutes.SignIn.name) {
                         inclusive = true
                     }
                 }
@@ -44,7 +68,7 @@ fun Navigation(
 
         composable(route = LoginRoutes.Signup.name) {
             SignUpScreen(onNavToHomePage = {
-                navController.navigate(HomeRoutes.Home.name) {
+                navController.navigate(NestedRoutes.Main.name) {
                     popUpTo(LoginRoutes.Signup.name) {
                         inclusive = true
                     }
@@ -53,9 +77,45 @@ fun Navigation(
                 navController.navigate(LoginRoutes.SignIn.name)
             }
         }
+    }
+}
 
-        composable(route = HomeRoutes.Home.name){
-            //HomeScreen()
+fun NavGraphBuilder.homeGraph(navController: NavHostController) {
+    navigation(
+        startDestination = HomeRoutes.Home.name,
+        route = NestedRoutes.Main.name
+    ) {
+        composable(HomeRoutes.Home.name) {
+            HomeScreen(
+                onNoteClicked = { noteId ->
+                    navController.navigate(HomeRoutes.Detail.name + "?id=$noteId") {
+                        launchSingleTop = true
+                    }
+                },
+                navToDetailPage = {
+                    navController.navigate(HomeRoutes.Detail.name)
+                }) {
+
+                navController.navigate(NestedRoutes.Login.name) {
+                    launchSingleTop = true
+                    popUpTo(0) {
+                        inclusive = true
+                    }
+                }
+
+            }
+        }
+
+        composable(
+            route = HomeRoutes.Detail.name + "?id={id}",
+            arguments = listOf(navArgument("id") {
+                type = NavType.StringType
+                defaultValue = ""
+            })
+        ) { entry ->
+            DetailScreen(noteId = entry.arguments?.getString("id") as String) {
+                navController.navigateUp()
+            }
         }
     }
 }
